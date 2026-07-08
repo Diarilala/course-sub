9cc9e9322018703ea9f62ead6be0ba8409729f55
Diarilala <hei.diarilala@gmail.com>
2026-07-08 20:39:54 +0300

    feat: add course entity

---
+++ src/main/java/com/agito/choom/repository/model/JCourse.java
@@ -0,0 +1,27 @@
+package com.agito.choom.repository.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "course")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class JCourse {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    private Instant startDate;
+    private Instant endDate;
+}
