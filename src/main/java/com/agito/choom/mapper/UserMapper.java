a2ad82cd5eca0e467df2f87b1c84c8f045e291d4
Diarilala <hei.diarilala@gmail.com>
2026-07-08 21:06:41 +0300

    feat: add user mapper

---
+++ src/main/java/com/agito/choom/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.agito.choom.mapper;
+
+import com.agito.choom.model.User;
+import com.agito.choom.repository.model.JUser;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserMapper {
+    public User toModel(JUser entity) {
+        return User.builder()
+                .id(entity.getId())
+                .firstName(entity.getFirstName())
+                .lastName(entity.getLastName())
+                .username(entity.getUsername())
+                .email(entity.getEmail())
+                .build();
+    }
+
+    public List<User> toModel(List<JUser> entities) {
+        return entities.stream().map(this::toModel).toList();
+    }
+
+    public JUser toEntity(JUser model) {
+        return JUser.builder()
+                .id(model.getId())
+                .firstName(model.getFirstName())
+                .lastName(model.getLastName())
+                .username(model.getUsername())
+                .email(model.getEmail())
+                .build();
+    }
+}
