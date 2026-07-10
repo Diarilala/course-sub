09bddbc0095bae69b24cd2b541bf7d96fe6dced7
Diarilala <hei.diarilala@gmail.com>
2026-07-10 22:43:28 +0300

    fix: add necessary annotations

--- src/main/java/com/agito/choom/endpoint/rest/controller/dto/SubscriptionRequest.java
+++ src/main/java/com/agito/choom/endpoint/rest/controller/dto/SubscriptionRequest.java
@@ -1,11 +1,13 @@
 package com.agito.choom.endpoint.rest.controller.dto;
 
 import java.util.UUID;
-import lombok.Builder;
-import lombok.Getter;
+
+import lombok.*;
 
 @Builder
-@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class SubscriptionRequest {
   private UUID userId;
 }
